3f1e505555727643c9efca133e6907c0e49ded24
Jan Ziarkowski <jan.ziarkowski@gmail.com>
2022-11-04 16:43:36 +0100

    heh

--- Optymalizacja/main.cpp
+++ Optymalizacja/main.cpp
@@ -36,19 +36,23 @@ int main()
 void lab1()
 {
 
+	double x0 = 69.69, d = 2.137, epsilon = 0.00001, gamma = 1e-40;
+
 	double* p = new double[2];
 	solution fibbobibo[100];
 	solution laga[100];
 	matrix min1 = 1000;
 	matrix min2 = 1000;
 	srand(time(NULL));
+
 	for (int i = 0; i < 100; i++)
 	{
-		double x0 = rand() % 1001 + 1;
+		//double x0 = rand() % 101 + 1;
 		p = expansion(&f_celu, x0, 1, 2.5, 1000, 0, 0);
 		cout << x0 << "," << p[0] << "," << p[1] << endl;
-		fibbobibo[i] =  fib(f_celu, p[0], p[1], 0.00001, 0, 0);
-		laga[i] = lag(f_celu, p[0], p[1], 0.00001, 1e-7, 0, 0);
+		fibbobibo[i] =  fib(f_celu, p[0], p[1], epsilon, 0, 0);
+		laga[i] = lag(f_celu, p[0], p[1], epsilon, gamma, 0, 0);
+		x0 += d;
 	}
 	cout << "FIBBOBIBO" << endl;
 	for (int i = 0; i < 100; i++)
