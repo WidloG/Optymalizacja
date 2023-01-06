3df81918cbbe23be8587da9f727e3c82fdc1e695
RobertZ00 <robert.zachwieja.00@gmail.com>
2023-01-06 22:39:26 +0100

    koniec aktu 1

--- Optymalizacja/main.cpp
+++ Optymalizacja/main.cpp
@@ -164,7 +164,7 @@ void lab4()
 	//Funkcja testowa
 	ofstream S;
 	S.open("wyniki.txt");
-	double epsilon = 1e-3, h = .05;
+	double epsilon = 1e-3, h = -1;
 	cout << h << endl << endl;
 	int Nmax = 5000;
 	matrix x0;
